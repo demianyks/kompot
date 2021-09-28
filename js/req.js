07508431973359dc7f2e60b5aab061f7f74de829
DemianykS <demianyk.s@yahoo.com>
2021-09-28 17:01:37 +0300

    some fixes SMTP

---
+++ js/req.js
@@ -0,0 +1,25 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded', function () {
+	const form = document.getElementById('form')
+	form.addEventListener('submit', formSend)
+
+	async function formSend(e) {
+		e.preventDefault()
+
+		let formData = new FormData(form)
+		
+		let response = await fetch('sendmail.php',{
+			method: 'POST',
+			body: formData
+		});
+		if(response.ok){
+			let result = await response.json()
+			alert(result.message)
+			form.reset()
+		} else{
+			alert('Помилка')
+		}
+	}
+
+})
--- sendmail.php
+++ sendmail.php
@@ -5,14 +5,24 @@ use PHPMailer\PHPMailer\Exception;
 
 require 'phpmailer/src/Exception.php';
 require 'phpmailer/src/PHPMailer.php';
+require 'phpmailer/src/SMTP.php';
 
 $email = new PHPMailer(true);
+$email->isSMTP();
+$email->Host = 'smtp.gmail.com'; // Which SMTP server to use.
+$email->Port = 587; // Which port to use, 587 is the default port for TLS security.
+$email->SMTPSecure = 'tls'; // Which security method to use. TLS is most secure.
+$email->SMTPAuth = true; // Whether you need to login. This is almost always required.
+$email->Username = 'kompot.uz.ua@gmail.com'; // Your Gmail address.
+$email->Password = 'pdwhjeumwxgzoisd'; // Your Gmail login password or App Specific Password.
+
 $email->CharSet='UTF-8';
 $email->setLanguage('ru','phpmailer/language/');
 $email->isHTML(true);
 
 $email->setFrom('kompot.uz.ua@gmail.com','KOMPOT');
 $email->addAddress("kompot.uz.ua@gmail.com");
+
 $email->Subject='Перезвоніть мені';
 
 
@@ -33,6 +43,6 @@ if(!$email->send()){
 
 $response = ['message' => $message];
 
-// header('Content-type: application/json');
+header('Content-type: application/json');
 echo json_encode($response);
 ?>
